a57bd51d3635ca25c0fe7511383813bc49013e5a
Matias <matias.b.dev@gmail.com>
2023-04-13 00:20:36 -0300

    agrege los titulos de las columnas y tambien la funcion para borrar los campos.

--- src/script.js
+++ src/script.js
@@ -4,35 +4,54 @@ let numTarea = 1;
 var tareas = [];
 
 function iniciar() {
-    let btnAgregar = document.getElementById("btnAgregar");
+  let btnAgregar = document.getElementById("btnAgregar");
 
-    btnAgregar.addEventListener("click", agregarTarea);
-
-};
+  btnAgregar.addEventListener("click", agregarTarea);
+}
 
 function agregarTarea(event) {
-    event.preventDefault();
-    let nombre = document.getElementById("txtNombre").value;
-    let descripcion = document.getElementById("txtArea").value;
-    let tarea = {
-        nombre: nombre,
-        descripcion: descripcion,
-        estado: true,
-    };
-    tareas.push(tarea); // Agregar tarea al arreglo
-    mostrarTabla(); // Actualizar tabla con todas las tareas
-};
+  event.preventDefault();
+  let nombre = document.getElementById("txtNombre").value;
+  let descripcion = document.getElementById("txtArea").value;
+  let tarea = {
+    nombre: nombre,
+    descripcion: descripcion,
+    estado: true,
+  };
+  tareas.push(tarea); // Agregar tarea al arreglo
+  mostrarTabla(); // Actualizar tabla con todas las tareas
+  limpiarCampos(); //Limpia los campos del formulario Agregar.
+}
 
 function mostrarTabla() {
-    var tabla = document.getElementById("tablaTareas");
-    tabla.innerHTML = ""; // Limpiar contenido actual de la tabla
-    for (let i = 0; i < tareas.length; i++) {
-        var fila = tabla.insertRow();
-        var celdaNombre = fila.insertCell();
-        var celdaDescripcion = fila.insertCell();
-        var celdaEstado = fila.insertCell();
-        celdaNombre.textContent = tareas[i].nombre;
-        celdaDescripcion.textContent = tareas[i].descripcion;
-        celdaEstado.textContent = tareas[i].estado ? "Completado" : "Pendiente";
-    }
-};
+  var tabla = document.getElementById("tablaTareas");
+  tabla.innerHTML = ""; // Limpiar contenido actual de la tabla
+  cargarNombresColumnas(); //Carga el nombre de cada columna despues de borrar la tabla.
+  for (let i = 0; i < tareas.length; i++) {
+    var fila = tabla.insertRow();
+    var celdaId = fila.insertCell();
+    var celdaNombre = fila.insertCell();
+    var celdaDescripcion = fila.insertCell();
+    var celdaEstado = fila.insertCell();
+    celdaId.textContent = i + 1;
+    celdaNombre.textContent = tareas[i].nombre;
+    celdaDescripcion.textContent = tareas[i].descripcion;
+    celdaEstado.textContent = tareas[i].estado ? "Completado" : "Pendiente";
+  }
+}
+function cargarNombresColumnas() {
+  var tabla = document.getElementById("tablaTareas");
+  var fila = tabla.insertRow();
+  var celdaId = fila.insertCell();
+  var celdaNombre = fila.insertCell();
+  var celdaDescripcion = fila.insertCell();
+  var celdaEstado = fila.insertCell();
+  celdaId.textContent = "Id";
+  celdaNombre.textContent = "Nombre";
+  celdaDescripcion.textContent = "descripcion";
+  celdaEstado.textContent = "Estado";
+}
+function limpiarCampos() {
+  document.getElementById("txtNombre").value = "";
+  document.getElementById("txtArea").value = "";
+}
